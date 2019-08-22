35ac78f68f8222ec1ecfab2e2d3d95e367312afa
rakjido <ndr.zenn@gmail.com>
2019-08-22 17:47:09 +0900

    2019.08.22

---
+++ apps.js
@@ -0,0 +1,16 @@
+// http
+const http = require('http');
+const routes = require('./routes');
+
+const server = http.createServer(routes);
+
+server.listen(3000);
+
+// https
+
+// fs
+
+// path
+
+// os
+
