7995ba66aa73235935d6f305c3111fe904c86ef0
rakjido <ndr.zenn@gmail.com>
2019-10-10 19:38:00 +0900

    2019.10.10

--- routes/admin.js
+++ routes/admin.js
@@ -2,27 +2,15 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productController = require('../controller/products');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-        pageTitle: 'Shop', 
-        path: '/admin/add-product', 
-        activeAddProduct: true,
-        productCSS: true 
-    });
-});
+router.get('/add-product', productController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productController.postAddProduct);
 
 exports.routes = router;
-exports.products = products;
+ 
